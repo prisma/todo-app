62b730f9037d909d98e95a80191f0631343e1122
Ankur Datta <64993082+ankur-arch@users.noreply.github.com>
2025-09-24 18:51:32 +0600

    fix: move to QC

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,25 @@
 import "dotenv/config";
-import { withAccelerate } from "@prisma/extension-accelerate";
-import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
+import { PrismaPg } from "@prisma/adapter-pg";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { PrismaClient } from "../generated/client/client";
+
+const instantiatePrisma = () => {
+  const connectionString = `${process.env.DATABASE_URL}`;
+  const adapter = new PrismaPg({ connectionString });
+  return new PrismaClient({ adapter }).$extends(withAccelerate());
 };
 
-const prisma =
-  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
+const globalForPrisma = global as unknown as {
+  prisma: ReturnType<typeof instantiatePrisma>;
+};
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = globalForPrisma.prisma || instantiatePrisma();
+} else {
+  globalForPrisma.prisma = instantiatePrisma();
+}
+
+const prisma = globalForPrisma.prisma;
 
 export { prisma };
